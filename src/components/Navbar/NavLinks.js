6bb2b0e288307f94460a42f7b481b1011f806f02
Bablukmr <bablukmr800@gmail.com>
2023-09-08 20:18:35 +0530

    sublink

---
+++ src/components/Navbar/NavLinks.js
@@ -0,0 +1,117 @@
+
+export  const links = [
+    {
+      name: 'men',
+      submenu: true,
+      sublinks: [
+        {
+          Head: "Topwear",
+          sublink: [
+            { name: 'T-shirt', link: '/' },
+            { name: 'Shirt', link: '/' },
+            { name: 'Jeans', link: '/' },
+            { name: 'Jackets', link: '/' },
+            { name: 'Accessories', link: '/' },
+          ],
+        },
+        {
+          Head: "Bottom Ware",
+          sublink: [
+            { name: 'T-shirt', link: '/' },
+            { name: 'Shirt', link: '/' },
+            { name: 'Jeans', link: '/' },
+            { name: 'Jackets', link: '/' },
+            { name: 'Accessories', link: '/' },
+          ],
+        },
+        {
+          Head: "Topwear",
+          sublink: [
+            { name: 'T-shirt', link: '/' },
+            { name: 'Shirt', link: '/' },
+            { name: 'Jeans', link: '/' },
+            { name: 'Jackets', link: '/' },
+            { name: 'Accessories', link: '/' },
+          ],
+        },
+        {
+          Head: "Topwear",
+          sublink: [
+            { name: 'T-shirt', link: '/' },
+            { name: 'Shirt', link: '/' },
+            { name: 'Jeans', link: '/' },
+            { name: 'Jackets', link: '/' },
+            { name: 'Accessories', link: '/' },
+          ],
+        },
+      ],
+    },
+    { name: 'women',submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      },
+      {
+        Head: "Bottom Ware",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      },
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      },
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      },
+    ] },
+    { name: 'kid', submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      },
+      {
+        Head: "Bottom Ware",
+        sublink: [
+          { name: 'T-shirt', link: '/' },
+          { name: 'Shirt', link: '/' },
+          { name: 'Jeans', link: '/' },
+          { name: 'Jackets', link: '/' },
+          { name: 'Accessories', link: '/' },
+        ],
+      }
+      
+    ]},
+    { name: 'Cloth' },
+  ];
--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -1,83 +1,43 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React, { useState } from "react";
+import { links } from "./NavLinks";
 
 export default function NavLinks() {
-  const links = [
-    {
-      name: 'men',
-      submenu: true,
-      sublinks: [
-        {
-          Head: "Topwear",
-          sublink: [
-            { name: 'T-shirt', link: '/' },
-            { name: 'Shirt', link: '/' },
-            { name: 'Jeans', link: '/' },
-            { name: 'Jackets', link: '/' },
-            { name: 'Accessories', link: '/' },
-          ],
-        },
-        {
-          Head: "Bottom Ware",
-          sublink: [
-            { name: 'T-shirt', link: '/' },
-            { name: 'Shirt', link: '/' },
-            { name: 'Jeans', link: '/' },
-            { name: 'Jackets', link: '/' },
-            { name: 'Accessories', link: '/' },
-          ],
-        },
-        {
-          Head: "Topwear",
-          sublink: [
-            { name: 'T-shirt', link: '/' },
-            { name: 'Shirt', link: '/' },
-            { name: 'Jeans', link: '/' },
-            { name: 'Jackets', link: '/' },
-            { name: 'Accessories', link: '/' },
-          ],
-        },
-        {
-          Head: "Topwear",
-          sublink: [
-            { name: 'T-shirt', link: '/' },
-            { name: 'Shirt', link: '/' },
-            { name: 'Jeans', link: '/' },
-            { name: 'Jackets', link: '/' },
-            { name: 'Accessories', link: '/' },
-          ],
-        },
-      ],
-    },
-    { name: 'women' },
-    { name: 'kid' },
-    { name: 'men' },
-  ];
+  const [heading, SetHeading] = useState("");
+  const [subHeading, SetSubHeading] = useState("");
 
   return (
     <>
       {links.map((link, index) => (
         <div key={index}>
-          <div className='px-3 text-left md:cursor-pointer '>
-            <h1 className='py-7'>{link.name}</h1>
+          <div className="px-3 text-left md:cursor-pointer group ">
+            <h1
+              className="py-7"
+              onClick={() =>
+                heading !== link.name ? SetHeading(link.name) : SetHeading("")
+              }
+            >
+              {link.name}
+            </h1>
             {link.submenu && (
               <div>
-                <div className='absolute top-20 '>
-                  <div className='py-1.5'>
-                    <div className='w-4 h-4 bg-white absolute  rotate-45'></div>
+                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
+                  <div className="py-1.5">
+                    <div className="w-4 h-4 bg-white absolute  rotate-45"></div>
                   </div>
-                  <div className=' bg-white ml-[-1rem] p-4 grid grid-cols-3 gap-6' >
+                  <div className=" bg-white ml-[-1rem] w-full p-4 flex gap-5 flex-wrap">
+                    {/* grid grid-cols-3 gap-6 */}
                     {link.sublinks.map((item, id) => (
-                      <div className='p-3.5' key={id}>
-                        <h1 className='text-lg font-semibold '>{item.Head}</h1>
+                      <div className="p-3.5" key={id}>
+                        <h1 className="text-lg font-semibold ">{item.Head}</h1>
                         {item.sublink.map((slink, sid) => (
                           <li
-                            className='text-sm text-gray-600 my-2.5 '
+                            className="text-sm text-gray-600 my-2.5 "
                             key={sid}
                           >
                             <Link
                               href={slink.link}
-                              className='hover:text-[#4A3AFF] hover:border-b-2'
+                              className="hover:text-[#4A3AFF] hover:border-b-2"
                             >
                               {slink.name}
                             </Link>
@@ -89,6 +49,41 @@ export default function NavLinks() {
                 </div>
               </div>
             )}
+          </div>
+          {/* Mobile  submenue */}
+          <div
+            className={`
+          ${heading === link.name ? "md:hidden" : "hidden"}
+          `}
+          >
+            {/* sublink */}
+            {link.sublinks?.map((slinks, id) => (
+              <div key={id}>
+                <div>
+                  <h1
+                    className="py-4 pl-7 font-semibold md:pr-0 pr-5"
+                    onClick={() =>
+                      subHeading !== subHeading.Head
+                        ? SetSubHeading(slinks.Head)
+                        : SetSubHeading("")
+                    }
+                  >
+                    {slinks.Head}
+                  </h1>
+                  <div
+                    className={`${
+                      subHeading === slinks.Head ? "md:hidden" : "hidden"
+                    }`}
+                  >
+                    {slinks.sublink.map((slink, sid) => (
+                      <li className="py-3 pl-14" key={sid}>
+                        <Link href={slink.link}>{slink.name}</Link>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}{" "}
           </div>
         </div>
       ))}
