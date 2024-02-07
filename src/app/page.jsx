020a8aeaf21fe5a4a5ad0ce7abb70bf2347c10c2
Bablukmr <bablukmr800@gmail.com>
2024-02-07 20:38:25 +0530

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,8 @@ import Button from "@/components/Navbar/Button";
 export default function Home() {
   
   return (
-   <div className="bg-slate-500 h-[70vh]">
- <h1 className="pt-80">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ullam expedita deserunt quo sit nobis assumenda inventore illo aliquid! Placeat vero consectetur maiores assumenda doloremque harum quasi unde ab ex?</h1>
-   <Button/>
-   </div>
+  <div>
+    
+  </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from 'next/image'
 import React, { useState } from 'react'
-import logo from '../../assets/logos.png'
+import logo from '../../assets/harinagarsugar.png'
 import Link from 'next/link'
 import NavLinks from './NavLinks'
 import Button from './Button'
@@ -11,44 +11,56 @@ import CloseIcon from '@mui/icons-material/Close';
 function Navbar() {
     const [open,setOpen]=useState(false)
     return (
-        <nav className='bg-white shadow-lg'>
-            <div className='flex items-center font-medium justify-around '>
-                <div className='z-50 p-2 md:w-auto w-full flex justify-between'>
+        <nav className='bg-white shadow-lg fixed top-0 w-full'>
+            <div className='flex items-center font-semibold md:mx-10 text-base justify-between'>
+                <div className='z-50 p-2 md:w-[15%] w-full flex justify-between'>
                     <Link href={'/'}><Image src={logo} width={180} alt='logo' className='md:cursor-pointer h-14' /></Link>
               <div className='p-1.5 bg-slate-300 m-3 rounded-md border-neutral-950 border-1 md:hidden' onClick={()=>setOpen(!open)}>
-              { open?<CloseIcon/>:<MenuIcon/>}
+                     { open?<CloseIcon/>:<MenuIcon/>}
               </div>
                 </div>
-                <ul className='md:flex hidden uppercase items-center gap-8 font-[Poppins]'>
-                <NavLinks />
+                <ul className='md:flex w-[90%] hidden uppercase justify-end items-center  font-[Montserrat]'>
+                
                     <li>
                         <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>About</Link>
                     </li>
+                    <NavLinks />
                     <li>
-                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>CAREER</Link>
+                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>Cogen</Link>
                     </li>
-                    
+                    <li>
+                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>Fertilizer</Link>
+                    </li>
+                    <li>
+                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>Contact</Link>
+                    </li>
                 </ul>
-                <div className=' lg:block hidden'>
+                {/* <div className=' lg:block hidden'>
                     <Button />
-                </div>
-                {/* Mobive nav */}
-                <ul className={`md:hidden bg-white absolute w-full h-full bottom-0 py-24 pl-4 
+                </div> */}
+               
+            </div>
+
+             {/* Mobive nav */}
+             <ul className={`md:hidden bg-white absolute w-full h-full bottom-0 pt-40 pl-4 
                 duration-500 ${open ? 'left-0':'left-[-100%]'}
                 `}>
-                     <NavLinks />
+                    
                     <li>
                         <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>About</Link>
                     </li>
+                    <NavLinks />
                     <li>
-                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>CAREER</Link>
+                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>Cogen</Link>
+                    </li>
+                    <li>
+                        <Link className='py-7 inline-block px-3 hover:text-blue-600' href={'/'}>Contact</Link>
                     </li>
                    
-                    <div className='py-5'>
+                    {/* <div className='py-5'>
                         <Button/>
-                    </div>
+                    </div> */}
                 </ul>
-            </div>
         </nav>
     )
 }
